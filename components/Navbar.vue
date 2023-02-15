b8f5398927d95027c783f1d82055b3e59feeed0e
Luc1ous <ardianilyas@gmail.com>
2023-02-15 20:34:11 +0700

    adding skills stack section and footer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,8 +9,8 @@
 <template>
   <nav class="w-full h-[80px] bg-white dark:text-white flex justify-between items-center fixed top-0 right-0 backdrop-filter backdrop-blur-lg bg-opacity-30 z-20">
     <div class="container w-full flex justify-between items-center">
-      <div class="text-2xl font-bold font-dm-serif">AI</div>
-      <button @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark' )" class="bg-white dark:bg-slate-800 p-[12px] rounded-full hover:scale-110 transition ease-in-out">
+      <div class="text-3xl font-bold font-dm-serif">AI</div>
+      <button @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark' )" class="bg-[#f25042] text-white dark:bg-slate-800 p-[12px] rounded-full hover:scale-110 transition ease-in-out">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" v-if="$colorMode.value != 'dark'">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
         </svg>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,69 @@
 <template>
-  <div class="font-nunito selection:bg-violet-800 selection:text-slate-100 selection:p-12">
+  <div class="font-nunito bg-[#fffffe] selection:bg-violet-800 selection:text-slate-100 selection:p-12">
     <Navbar />
+
+    <!-- Hero Section -->
     <section>
-      <div class="relative w-full h-screen bg-slate-200 dark:bg-slate-900 dark:text-white flex justify-center items-center">
+      <div class="relative w-full h-screen bg-[#eaddcf] dark:bg-[#010101] dark:text-white flex justify-center items-center">
         <div class="lg:w-3/4 w-full text-center p-4 space-y-4 z-10">
-          <h1 class="text-7xl font-dm-serif font-semibold">Ardian Ilyas Fernanda</h1>
-          <p class="text-lg font-light text-slate-900 dark:text-white">Hello, I'am a Student that interesting about <span class="text-indigo-900 dark:text-indigo-300 underline underline-offset-4">Fullstack Web Developer</span></p>
+          <h1 class="text-7xl text-[#020826] dark:text-white font-dm-serif">Ardian Ilyas Fernanda</h1>
+          <p class="text-lg font-light text-[#716040] dark:text-white">Hello, I'am a Student that interesting about <span class="text-indigo-900 dark:text-indigo-300 underline underline-offset-4">Fullstack Web Developer</span></p>
         </div>
       </div>
     </section>
 
-    <section class="w-full min-h-screen dark:bg-slate-800 flex flex-col justify-center items-center">
+    <!-- Skill Section -->
+    <section class="w-full min-h-screen dark:bg-[#16161a] flex flex-col justify-center items-center">
       <div class="w-full my-20">
         <div class="text-center">
-          <h1 class="font-dm-serif text-5xl">Skill Stack</h1>
-          <p class="text-lg font-light text-slate-900 dark:text-white mt-4">Here is my skill stack</p>
+          <h1 class="font-dm-serif text-[#020826] dark:text-white text-5xl">Skills Stack</h1>
+          <p class="text-lg font-light text-[#716040] dark:text-white mt-4">Here is my skill stack</p>
         </div>
-        <div class="container w-full mt-16 grid grid-cols-3 place-items-center gap-x-8 gap-y-24">
-          <div class="">
+        <div class="container w-full mt-16 grid grid-cols-2 md:grid-cols-4 place-items-center gap-x-4 gap-y-24">
+          <a href="https://html.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/html-1.svg" alt="" width="80">
+          </a>
+          <div class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/css.svg" alt="" width="80">
+          </div>
+          <a href="https://www.php.net/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
             <img src="~/assets/img/php.svg" alt="" width="100">
-          </div>
-          <div class="">
+          </a>
+          <a href="https://www.javascript.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
             <img src="~/assets/img/javascript.svg" alt="" width="80">
-          </div>
-          <div class="">
-            <img src="~/assets/img/bootstrap.svg" alt="" width="80">
-          </div>
-          <div class="">
-            <img src="~/assets/img/tailwindcss.svg" alt="" width="80">
-          </div>
-          <div class="">
-            <img src="~/assets/img/vuejs.svg" alt="" width="80">
-          </div>
-          <div class="">
+          </a>
+          <a href="https://getbootstrap.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/bootstrap.svg" alt="" width="100">
+          </a>
+          <a href="https://tailwindcss.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/tailwindcss.svg" alt="" width="100">
+          </a>
+          <a href="https://laravel.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
             <img src="~/assets/img/laravel.svg" alt="" width="80">
-          </div>
-          <div class="">
-            <img src="~/assets/img/nuxt.svg" alt="" width="80">
-          </div>
+          </a>
+          <a href="https://vuejs.org/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/vuejs.svg" alt="" width="80">
+          </a>
+          <a href="https://nuxt.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/nuxt.svg" alt="" width="100">
+          </a>
+          <a href="https://www.mysql.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/mysql.svg" alt="" width="120">
+          </a>
+          <a href="https://www.figma.com/" target="_blank" class="grayscale hover:grayscale-0 transition ease-in-out duration-200 cursor-pointer">
+            <img src="~/assets/img/figma2.svg" alt="" width="60">
+          </a>
         </div>
       </div>
     </section>
+
+    <!-- Footer -->
+    <footer class="w-full h-36 bg-[#eaddcf] dark:bg-[#010101] flex justify-center items-center">
+      <p class="text-lg dark:text-white">Copyright &copy; {{ year }} all rights reserved</p>
+    </footer>
   </div>
 </template>
 
 <script setup>
-
+  const year = new Date().getFullYear()
 </script>
